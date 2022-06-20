72d172da6a6da66e1c6633e8ef8921b22c2f6453
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-06-20 14:00:44 -0300

    Add files via upload

---
+++ condicional2.js
@@ -0,0 +1,52 @@
+if (3 > 4){
+    console.log('3 é maior que 4')
+}else{
+    console.log('3 é menor que 4')
+}
+
+// Se o carro tiver mais que 5 anos de uso, é velho, se não, é novo.
+
+let idadeCarro = 5;
+if (idadeCarro >= 5){
+    console.log('O seu carro é velho')
+}else{
+    console.log('O seu carro é novo')
+}
+
+let nome = 'Rebec';
+
+
+
+if (nome != 'Rebeca'){
+    console.log('O seu nome é feio')
+}else{
+    console.log('O seu nome é bonito')
+}
+
+//prompt
+
+let numero = prompt('Dos numeros de 0 a 5. Em qual estou pensando?')
+
+if (numero == 4){
+     alert('Você acertou. Parabéns!!')
+}else{
+    alert('Você errou. Que pena :(')
+}
+
+let nota1 = parseInt(prompt('Nota de Html'))
+let nota2 = parseInt(prompt('Nota de Css'))
+let nota3 = parseInt(prompt('Nota de Js'))
+
+let media = (nota1+nota2+nota3)/3
+
+if (media >=7){
+    alert('Aprovado')  
+} else{
+    alert ('Reprovado, bb!!!!!!!!!!!!!!!!!!!!!!!!!')
+}
+  
+
+
+
+
+
